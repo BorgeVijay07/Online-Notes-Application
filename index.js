90a773c0765e4b4849f1e25d41a09e2a864c2f65
BorgeVijay07 <borgevijay11@gmail.com>
2021-06-19 19:15:41 +0530

    Added sign up functionality.

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 //Ajax Call for the sign up form
 //Once the form is submitted
+$("#signupform").submit(function(event){
     //prevent default php processing
+    event.preventDefault();
     //collect user inputs
+    var datatopost = $(this).serializeArray();
+    console.log(datatopost);
     //send them to signup.php using AJAX
+    $.ajax({
+        url: "signup.php",
+        type: "POST",
+        data: datatopost,
         //AJAX call successful: show error or success message
+        success: function(data){
+            if(data){
+                $("#signupMessage").html(data);
+            }
+        },
         //AJAX call fails: show Ajax call error
-
+        error: function(){
+            $("#signupMessage").html("<div class='alert alert-danger'>There was an error with the Ajax Call. Please try again later.</div>");
+        }
+    });
+});
+    
 //Ajax call for the login form
 //Once the form is submitted
     //prevent default php processing
